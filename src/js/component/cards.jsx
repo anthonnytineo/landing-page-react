94f30f7401d1b66576dee56d51c40026c65ced15
Nicolas Guzman <guzma.r.nicolas@gmail.com>
2021-04-30 20:46:40 -0300

    cards & footer

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+export function Cards() {
+	return (
+		<div className="countaier">
+			<div className="row">
+				<div className="card col-3">
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a longer card with supporting text below as
+							a natural lead-in to additional content. This
+							content is a little bit longer.
+						</p>
+						<p className="card-text">
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find out More!
+						</a>
+					</div>
+				</div>
+				<div className="card col-3">
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This card has supporting text below as a natural
+							lead-in to additional content.Solo tiene menos
+							texto.
+						</p>
+						<p className="card-text">
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find out More!
+						</a>
+					</div>
+				</div>
+				<div className="card col-3">
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a wider card with supporting text below as a
+							natural lead-in to additional content. This card has
+							even longer content than the first to show that
+							equal height action.
+						</p>
+						<p className="card-text">
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find out More!
+						</a>
+					</div>
+				</div>
+				<div className="card col-3">
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							This is a longer card with supporting text below as
+							a natural lead-in to additional content. This
+							content is a little bit longer.
+						</p>
+						<p className="card-text">
+							<small className="text-muted">
+								Last updated 3 mins ago
+							</small>
+						</p>
+						<a href="#" className="btn btn-primary">
+							Find out More!
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export function Footer() {
+	return (
+		<footer className="py-5 bg-dark">
+			<div className="container">
+				<p className="m- text-center text-white">
+					the footer <footer></footer>
+				</p>
+			</div>
+		</footer>
+	);
+}
